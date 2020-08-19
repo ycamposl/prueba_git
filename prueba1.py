c40d04b6a8765d7a915bde38c7d8198ac050c28d
ycamposl <ycamposl@uni.pe>
2020-08-19 00:58:56 -0500

    modifique esto

--- prueba1.py
+++ prueba1.py
@@ -1,4 +1,4 @@
 import pandas as pd 
 
-df=pd.DataFrame({'v1':[1,2,3,4],'v2':['yerimen','antonio','campos','luyo']})
+df=pd.DataFrame({'v1':[1,2,3,4,5],'v2':['yerimen','antonio','campos','luyo','ximena']})
 print(df)
