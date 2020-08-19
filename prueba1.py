db7f3d782e5eaae07da4e92e1abfeee8a12cca34
ycamposl <ycamposl@uni.pe>
2020-08-19 01:29:37 -0500

    modificacion rama2

--- prueba1.py
+++ prueba1.py
@@ -1,4 +1,4 @@
 import pandas as pd 
 
-df=pd.DataFrame({'v1':[1,2,3,4,5],'v2':['yerimen','antonio','campos','luyo','ximena']})
+df=pd.DataFrame({'v1':[1,2,3,4,5,6],'v2':['yerimen','antonio','campos','luyo','ximena','kaori']})
 print(df)
